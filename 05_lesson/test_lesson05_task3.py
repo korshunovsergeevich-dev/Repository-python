184c8584cd5a83f0e01f1fde1ad3014cbf5072c1
korshunovsergeevich-dev <korshunovsergeevich@gmail.com>
2026-07-10 19:55:12 +0300

    Update test_lesson05_task3.py

--- 05_lesson/test_lesson05_task3.py
+++ 05_lesson/test_lesson05_task3.py
@@ -10,12 +10,12 @@ def test_multiple_elements():
 
     links = driver.find_elements(By.TAG_NAME, "a")
 
-    assert len(links) == 9, {len(links)}
+    assert len(links) == 9, f"Expected 9, got {len(links)}"
 
     for link in links:
         assert link.is_displayed()
 
-    first_link_text = links.text
+    first_link_text = links[0].text
     assert "1" in first_link_text
 
     driver.quit()
