5d5c874ce994c9fb6c11757576503dbf98d8cf28
korshunovsergeevich-dev <korshunovsergeevich@gmail.com>
2026-07-15 18:56:37 +0300

    Update test_lesson06_task2.py

--- 06_lesson/test_lesson06_task2.py
+++ 06_lesson/test_lesson06_task2.py
@@ -1,8 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
+
 
 def test_session_storage_auth():
     driver = webdriver.Chrome()
